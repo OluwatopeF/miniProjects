a178c25b78a88e4e7ff453d9d1d815054ee0c2c6
OluwatopeF <topeumar@gmail.com>
2025-05-20 15:37:30 -0400

    update 5/20/2025

--- sites/fortuneTeller/fortune.js
+++ sites/fortuneTeller/fortune.js
@@ -1,19 +1,56 @@
-// 
+//  random number 
 var num = 0;
 
+// elements 
+const answer = document.getElementById("result");
+
+var yes = document.createTextNode("Your fortune WILL happen");
+var no = document.createTextNode("Your fortune will NOT happen");
+var maybe = document.createTextNode("Hmmmmmmmmm................?");
+
+
+// tellFortune shows your fortune
 function tellFortune(num){
-    num =Math.floor(Math.random() * 3) + 1;
+    num = Math.floor(Math.random() * 3) + 1;
+    console.log(num)
 
     if(num == 1){
         console.log("your fortune will happen");
-        // return answer
+        // answer.removeChild(no, maybe);
+        answer.appendChild(yes);
+        
     }
     else if(num == 2){
         console.log("your fortune will not happen");
+        // answer.removeChild(yes, maybe);
+        answer.appendChild(no);
     }
     else{
         console.log("IDK??..... Maybe?");
+        // answer.removeChild(yes, no);
+        answer.appendChild(maybe);
+    }
+};
+
+// clearFortune: happens after first fortune
+// removes the appened child before replacing
+function clearFortune(){
+    if(answer.childNodes.length > 0){
+        console.log("removing old fortune");
+        answer.removeChild(answer.firstChild);
+    }
+    else{
+        console.log("Fortune Incoming");
+        // have an animation playing or smth
     }
 }
 
+function combo(){
+    clearFortune();
+    tellFortune(num);
+    
+}
+
+// events
+document.getElementById("fortuneBtn").addEventListener("click", combo);
 
