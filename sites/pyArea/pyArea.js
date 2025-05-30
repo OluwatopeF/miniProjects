15345687d09eecb4df5727034b12115507f88366
OluwatopeF <topeumar@gmail.com>
2025-05-30 14:23:31 -0400

    update 5/30/2025

--- sites/pyArea/pyArea.js
+++ sites/pyArea/pyArea.js
@@ -1,17 +1,32 @@
-var aInput = document.getElementById("inputA").value;
-var bInput = document.getElementById("inputB").value;
-
-let area = aInput * bInput;
-
+// calculate the area
 function getArea(){
-    console.log(area);
+    var aInput = document.getElementById('inputA').value;
+    var bInput = document.getElementById('inputB').value;
+    let area = aInput * bInput;
+    console.log("a is :", aInput);
+    console.log("b is :", bInput);
+    console.log("area is: ", area);
+    document.getElementById("resultNum").innerHTML = area;
 }
+
+// to move to next input
+document.getElementById('inputA').addEventListener("keypress", function (event){
+    if(event.key === 'Enter'){
+        event.preventDefault();    
+        const currentElement = document.activeElement;
+        const inputs = Array.from(document.querySelectorAll("input, textarea, select"));
+        const currentIndex = inputs.indexOf(currentElement);
+        if (currentIndex >= 0 && currentIndex < inputs.length - 1) {
+           inputs[currentIndex + 1].focus();
+         }
+        }
+});
 
 // secret button: pressing enter clicks and sends result
 document.getElementById('inputB').addEventListener("keypress", function (event){
     if(event.key === 'Enter'){
-        // console.log("works");
         document.getElementById("inputSend").click();
     }
 });
 
+document.getElementById("inputSend").addEventListener("click", getArea);
